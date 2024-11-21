70452dc7d2a2bf1e188ba9ec2d08c62360825dc0
Shush <shushanik@neographgames.com>
2024-11-21 16:39:41 +0300

    Added Menu widget

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
@@ -27,7 +27,10 @@ public class MultiplayerSessions : ModuleRules
 			{
 				"Core",
 				"OnlineSubsystem",
-				"OnlineSubsystemSteam"
+				"OnlineSubsystemSteam",
+				"Slate",
+				"UMG",
+				"SlateCore"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
@@ -40,6 +43,7 @@ public class MultiplayerSessions : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"UMG"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
